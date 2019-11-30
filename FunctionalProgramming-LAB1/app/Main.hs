277708fc073ecfe978f5b53ff0bc7620578c7c66
Vdovicenco Alexandr <vdovicenco.alexandr@gmail.com>
2019-11-30 19:59:34 +0200

    Add reading of input and filtering according task condition

--- FunctionalProgramming-LAB1/app/Main.hs
+++ FunctionalProgramming-LAB1/app/Main.hs
@@ -2,5 +2,14 @@ module Main where
 
 import Lib
 
+parseInput :: String -> [Int]
+parseInput input = map read $ words input
+
+filterInput :: [Int] -> [Int]
+filterInput = filter (\number -> number > 0 && number < 100)
+
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "Input list elements:"
+  input <- getLine
+  print $ filterInput $ parseInput input
